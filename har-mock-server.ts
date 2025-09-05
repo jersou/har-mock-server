24e529b40afd7306133e0c3f2f401bdc4b607355
Jerem <jersou@gmail.com>
2025-09-06 01:59:26 +0200

    fix: fix types

--- har-mock-server.ts
+++ har-mock-server.ts
@@ -1,16 +1,18 @@
 #!/usr/bin/env -S deno run --allow-net --allow-read
 import { alias, cliteRun, help, usage } from "jsr:@jersou/clite@0.7.7";
+type Request = { url: string; method: string };
+type Response = { status: number; data: any; content: { text: string } };
+type Entry = { request: Request; response: Response };
 
 // ignore the hostname
-function compareReq(request1: string, request2: string) {
+function compareReq(request1: Request, request2: Request) {
   const url1 = new URL(request1.url);
   const url2 = new URL(request2.url);
   return url1.search === url2.search && url1.pathname === url2.pathname &&
     url1.hash === url2.hash && request1.method === request2.method;
 }
 
-// deno-lint-ignore no-explicit-any
-function handleRequest(request: Request, harEntries: any[]) {
+function handleRequest(request: Request, harEntries: Entry[]) {
   const entry = harEntries.find((entry) => compareReq(entry.request, request));
   if (entry) {
     console.log(`[${entry.response.status}] ${request.method} ${request.url} `);
@@ -23,7 +25,7 @@ function handleRequest(request: Request, harEntries: any[]) {
   }
 }
 
-async function serveHar(port, filePath) {
+async function serveHar(port: number, filePath: string) {
   const harStr = await Deno.readTextFile(filePath);
   const harEntries = JSON.parse(harStr).log.entries;
   await Deno.serve({ port }, (request) => handleRequest(request, harEntries));
